49172207c15debe94e76497d43bf87ba59d359c6
Tasin Ahammed <freelancertasin507@gmail.com>
2023-05-04 23:01:35 +0600

    environment set complete

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_-oW7mBDp5d91yFlV9wFUBuJd4hJjRhI",
-  authDomain: "chefs-recipes-hunter.firebaseapp.com",
-  projectId: "chefs-recipes-hunter",
-  storageBucket: "chefs-recipes-hunter.appspot.com",
-  messagingSenderId: "836537359365",
-  appId: "1:836537359365:web:26427f27e0a0a8ae0a906c"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId ,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
